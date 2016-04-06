e6bac580ad28b3c5ea3f6f4b1fcbfa0b43c069d6
Daniel-Alievsky <daniel.alievsky@gmail.com>
2016-04-06 16:08:31 +0300

    Adding -Xmx8g to the test launcher

--- pyramid-algart-based/test-launcher/LaunchJavaTest.cmd
+++ pyramid-algart-based/test-launcher/LaunchJavaTest.cmd
@@ -1,1 +1,1 @@
-java -classpath api/*;.m2/* net.algart.pyramid.standard.tests.OpenSourceImageFileAccessTest %1
+java -Xmx8g -classpath api/*;.m2/* net.algart.pyramid.standard.tests.OpenSourceImageFileAccessTest %1
